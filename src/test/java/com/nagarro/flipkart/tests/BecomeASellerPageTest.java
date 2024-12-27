3c7691b5a093925c17c05527b256a07301ded355
Shivangi Gupta <108882400+shivi68@users.noreply.github.com>
2024-12-27 15:14:23 +0530

    Update BecomeASellerPageTest.java

--- src/test/java/com/nagarro/flipkart/tests/BecomeASellerPageTest.java
+++ src/test/java/com/nagarro/flipkart/tests/BecomeASellerPageTest.java
@@ -1,29 +1,29 @@
-package com.nagarro.flipkart.tests;
+// package com.nagarro.flipkart.tests;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+// import org.testng.Assert;
+// import org.testng.annotations.Test;
 
-import com.nagarro.flipkart.utils.WebDriverFactory;
+// import com.nagarro.flipkart.utils.WebDriverFactory;
 
-public class BecomeASellerPageTest extends WebDriverFactory {
+// public class BecomeASellerPageTest extends WebDriverFactory {
 	
-	@Test
-	public void testBecomeASeller() {
-		createTest("testBecomeASeller");
-		try {
-			logger.info("Starting test: testBecomeASeller");
-			test.info("Starting test: testBecomeASeller");
+// 	@Test
+// 	public void testBecomeASeller() {
+// 		createTest("testBecomeASeller");
+// 		try {
+// 			logger.info("Starting test: testBecomeASeller");
+// 			test.info("Starting test: testBecomeASeller");
 			
-			homePage.navigateToBecomeASellerPage();
-			test.info("Navigated to the Become A Seller Page.");
+// 			homePage.navigateToBecomeASellerPage();
+// 			test.info("Navigated to the Become A Seller Page.");
 
-			Assert.assertTrue(homePage.isPageDisplayed(homePage.getSellerDashboard()), "Seller Page is not displayed.");
-			test.pass("Seller Page is displayed successfully.");
-		    logger.info("Seller Page is displayed successfully.");
+// 			Assert.assertTrue(homePage.isPageDisplayed(homePage.getSellerDashboard()), "Seller Page is not displayed.");
+// 			test.pass("Seller Page is displayed successfully.");
+// 		    logger.info("Seller Page is displayed successfully.");
 			
-		} catch (AssertionError e) {
-			logger.error("Test failed: Seller Page is not displayed: {}", e.getMessage());
-	        handleTestFailure("Test failed: " + e.getMessage());
-		}
-	}
-}
+// 		} catch (AssertionError e) {
+// 			logger.error("Test failed: Seller Page is not displayed: {}", e.getMessage());
+// 	        handleTestFailure("Test failed: " + e.getMessage());
+// 		}
+// 	}
+// }
